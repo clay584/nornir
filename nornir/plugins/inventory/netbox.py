aa0350760844fabf4e829e3023498b7c45cd84ab
Clay Curtis <clay584@gmail.com>
2020-02-21 11:22:37 -0500

    added secrets from netbox capability

--- nornir/plugins/inventory/netbox.py
+++ nornir/plugins/inventory/netbox.py
@@ -104,7 +104,6 @@ class NBInventory(Inventory):
         # Pass the data back to the parent class
         super().__init__(hosts=hosts, groups={}, defaults={}, **kwargs)
 
-
 class NetboxInventory2(Inventory):
     """
     Inventory plugin that uses `Netbox <https://github.com/netbox-community/netbox>`_ as backend.
@@ -116,6 +115,7 @@ class NetboxInventory2(Inventory):
     Environment Variables:
         * ``NB_URL``: Corresponds to nb_url argument
         * ``NB_TOKEN``: Corresponds to nb_token argument
+        * ``NB_PRIVATE_KEY``: Corresponds to nb_private_key
 
     Arguments:
         nb_url: Netbox url (defaults to ``http://localhost:8080``)
@@ -125,6 +125,8 @@ class NetboxInventory2(Inventory):
         flatten_custom_fields: Assign custom fields directly to the host's data attribute
             (defaults to False)
         filter_parameters: Key-value pairs that allow you to filter the Netbox inventory.
+        nb_private_key: Netbox user private key
+        nb_cred_role_slug: Netbox secret role slug for which type of credentials to pull in.
     """
 
     def __init__(
@@ -135,6 +137,8 @@ class NetboxInventory2(Inventory):
         ssl_verify: Union[bool, str] = True,
         flatten_custom_fields: bool = False,
         filter_parameters: Optional[Dict[str, Any]] = None,
+        nb_private_key: Optional[str] = None,
+        nb_cred_role_slug: Optional[str] = "login-creds",
         **kwargs: Any,
     ) -> None:
         filter_parameters = filter_parameters or {}
@@ -143,9 +147,65 @@ class NetboxInventory2(Inventory):
             "NB_TOKEN", "0123456789abcdef0123456789abcdef01234567"
         )
 
+        # Load in user private key from environment var or file load
+        nb_private_key = nb_private_key or os.environ.get("NB_PRIVATE_KEY", None)
+
+        if os.environ.get('NB_PRIVATE_KEY') is not None:
+            nb_private_key = os.environ.get('NB_PRIVATE_KEY')
+        elif os.environ.get('NB_PRIVATE_KEY') is None and nb_private_key:
+            with open(nb_private_key, 'r') as f:
+                nb_private_key = f.read()
+
         session = requests.Session()
         session.headers.update({"Authorization": f"Token {nb_token}"})
         session.verify = ssl_verify
+
+        # Fetch all secrets from Netbox
+        # Since the api uses pagination we have to fetch until no next is provided
+        if nb_private_key:
+            # Get session key first in order to get decrypted secrets
+
+            url = f"{nb_url}/api/secrets/get-session-key/"
+            nb_session_key: str = None
+
+            headers = {
+                'Connection': 'keep-alive',
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'DNT': '1',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-US,en;q=0.9',
+            }
+
+            payload = {
+                "private_key": nb_private_key
+            }
+            r = session.post(url, headers=headers, data=payload)
+
+            if not r.status_code == 200:
+                raise ValueError(f"Failed to get session key from Netbox instance {nb_url}")
+
+            # Add session key to cookie jar for session
+            session_key_cookie = r.json()
+            session.cookies.set(name="session_key", value=session_key_cookie.get("session_key"))
+
+
+            # Get all secrets
+
+            url = f"{nb_url}/api/secrets/secrets/?limit=0"
+            nb_secrets: List[Dict[str, Any]] = []
+
+            while url:
+                r = session.get(url)
+
+                if not r.status_code == 200:
+                    raise ValueError(f"Failed to get secrets from Netbox instance {nb_url}")
+
+                resp = r.json()
+                nb_secrets.extend(resp.get("results"))
+
+                url = resp.get("next")
 
         # Fetch all devices from Netbox
         # Since the api uses pagination we have to fetch until no next is provided
@@ -183,6 +243,24 @@ class NetboxInventory2(Inventory):
                     host["data"][cf] = value
                 host["data"].pop("custom_fields")
 
+            # Add secrets to host if it has one in Netbox
+            if nb_private_key:
+                # Filter secrets based on netbox secret role slug
+                f_secrets = [s for s in nb_secrets if s.get("role").get("slug") == nb_cred_role_slug]
+                # Add secrets to device if they exist
+                host_secret = [s for s in f_secrets if s.get("device").get("id") == dev.get("id")]
+                if host_secret:
+                    host_secret = host_secret[0]
+                    host["username"] = host_secret.get("name")
+                    host["password"] = host_secret.get("plaintext")
+
+            # management port from netbox config_context if it's available
+            if host.get("data").get("config_context").get("nornir").get("port"):
+                host["port"] = host.get("data").get("config_context").get("nornir").get("port")
+
+            # Attempt to add 'platform' based of value in 'slug'
+            host["platform"] = dev["platform"]["slug"] if dev["platform"] else None
+
             # Assign temporary dict to outer dict
             # Netbox allows devices to be unnamed, but the Nornir model does not allow this
             # If a device is unnamed we will set the name to the id of the device in netbox
@@ -190,3 +268,4 @@ class NetboxInventory2(Inventory):
 
         # Pass the data back to the parent class
         super().__init__(hosts=hosts, groups={}, defaults={}, **kwargs)
+
