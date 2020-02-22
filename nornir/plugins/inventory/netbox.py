a200484522ec3b8e88698812307124b70985c4e7
Clay Curtis <clay584@gmail.com>
2020-02-22 08:27:44 -0500

    updated with requested changes

--- nornir/plugins/inventory/netbox.py
+++ nornir/plugins/inventory/netbox.py
@@ -104,7 +104,6 @@ class NBInventory(Inventory):
         # Pass the data back to the parent class
         super().__init__(hosts=hosts, groups={}, defaults={}, **kwargs)
 
-
 class NetboxInventory2(Inventory):
     """
     Inventory plugin that uses `Netbox <https://github.com/netbox-community/netbox>`_ as backend.
@@ -126,20 +125,19 @@ class NetboxInventory2(Inventory):
         flatten_custom_fields: Assign custom fields directly to the host's data attribute
             (defaults to False)
         filter_parameters: Key-value pairs that allow you to filter the Netbox inventory.
-        nb_private_key: Netbox user private key
-        nb_cred_role_slug: Netbox secret role slug for which type of credentials to pull in.
+        nb_private_key: Netbox user private key file path
+        nb_cred_role: Netbox secret role name for which type of credentials to pull in.
     """
 
     def __init__(
         self,
         nb_url: Optional[str] = None,
         nb_token: Optional[str] = None,
-        use_slugs: bool = True,
         ssl_verify: Union[bool, str] = True,
         flatten_custom_fields: bool = False,
         filter_parameters: Optional[Dict[str, Any]] = None,
         nb_private_key: Optional[str] = None,
-        nb_cred_role_slug: Optional[str] = "login-creds",
+        nb_cred_role: Optional[str] = None,
         **kwargs: Any,
     ) -> None:
         filter_parameters = filter_parameters or {}
@@ -149,13 +147,11 @@ class NetboxInventory2(Inventory):
         )
 
         # Load in user private key from environment var or file load
-        nb_private_key = nb_private_key or os.environ.get("NB_PRIVATE_KEY", None)
-
-        if os.environ.get("NB_PRIVATE_KEY") is not None:
-            nb_private_key = os.environ.get("NB_PRIVATE_KEY")
-        elif os.environ.get("NB_PRIVATE_KEY") is None and nb_private_key:
+        if nb_private_key:
             with open(nb_private_key, "r") as f:
                 nb_private_key = f.read()
+        elif os.environ.get("NB_PRIVATE_KEY") is not None:
+            nb_private_key = os.environ.get("NB_PRIVATE_KEY")
 
         session = requests.Session()
         session.headers.update({"Authorization": f"Token {nb_token}"})
@@ -169,18 +165,8 @@ class NetboxInventory2(Inventory):
             url = f"{nb_url}/api/secrets/get-session-key/"
             nb_session_key: str = None
 
-            headers = {
-                "Connection": "keep-alive",
-                "Accept": "application/json, text/javascript, */*; q=0.01",
-                "DNT": "1",
-                "X-Requested-With": "XMLHttpRequest",
-                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "Accept-Encoding": "gzip, deflate",
-                "Accept-Language": "en-US,en;q=0.9",
-            }
-
             payload = {"private_key": nb_private_key}
-            r = session.post(url, headers=headers, data=payload)
+            r = session.post(url, data=payload)
 
             if not r.status_code == 200:
                 raise ValueError(
@@ -196,37 +182,13 @@ class NetboxInventory2(Inventory):
             # Get all secrets
 
             url = f"{nb_url}/api/secrets/secrets/?limit=0"
-            nb_secrets: List[Dict[str, Any]] = []
-
-            while url:
-                r = session.get(url)
-
-                if not r.status_code == 200:
-                    raise ValueError(
-                        f"Failed to get secrets from Netbox instance {nb_url}"
-                    )
-
-                resp = r.json()
-                nb_secrets.extend(resp.get("results"))
-
-                url = resp.get("next")
+            nb_secrets = self._get_paginated(nb_url, session)
 
         # Fetch all devices from Netbox
         # Since the api uses pagination we have to fetch until no next is provided
 
         url = f"{nb_url}/api/dcim/devices/?limit=0"
-        nb_devices: List[Dict[str, Any]] = []
-
-        while url:
-            r = session.get(url, params=filter_parameters)
-
-            if not r.status_code == 200:
-                raise ValueError(f"Failed to get devices from Netbox instance {nb_url}")
-
-            resp = r.json()
-            nb_devices.extend(resp.get("results"))
-
-            url = resp.get("next")
+        nb_devices = self._get_paginated(nb_url, session)
 
         hosts = {}
         for dev in nb_devices:
@@ -251,9 +213,7 @@ class NetboxInventory2(Inventory):
             if nb_private_key:
                 # Filter secrets based on netbox secret role slug
                 f_secrets = [
-                    s
-                    for s in nb_secrets
-                    if s.get("role").get("slug") == nb_cred_role_slug
+                    s for s in nb_secrets if s.get("role").get("name") == nb_cred_role
                 ]
                 # Add secrets to device if they exist
                 host_secret = [
@@ -270,13 +230,39 @@ class NetboxInventory2(Inventory):
                     host.get("data").get("config_context").get("nornir").get("port")
                 )
 
-            # Attempt to add 'platform' based of value in 'slug'
-            host["platform"] = dev["platform"]["slug"] if dev["platform"] else None
-
             # Assign temporary dict to outer dict
             # Netbox allows devices to be unnamed, but the Nornir model does not allow this
             # If a device is unnamed we will set the name to the id of the device in netbox
             hosts[dev.get("name") or dev.get("id")] = host
+
+    @classmethod
+    def _get_paginated(cls, nb_url, session):
+        url = f"{nb_url}/api/secrets/secrets/?limit=0"
+        items: List[Dict[str, Any]] = []
+
+        while url:
+            r = session.get(url)
+
+            if not r.status_code == 200:
+                if "secrets" in url:
+                    raise ValueError(
+                        f"Failed to get secrets from Netbox instance {nb_url}"
+                    )
+                elif "devices" in url:
+                    raise ValueError(
+                        f"Failed to get devices from Netbox instance {nb_url}"
+                    )
+                else:
+                    raise ValueError(
+                        f"Failed to get data from Netbox instance {nb_url}"
+                    )
+
+            resp = r.json()
+            items.extend(resp.get("results"))
+
+            url = resp.get("next")
+
+        return items
 
         # Pass the data back to the parent class
         super().__init__(hosts=hosts, groups={}, defaults={}, **kwargs)
